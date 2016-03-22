c3611ecce3f571caa703e0b38a0914565c8c6211
sivanalkalay <sivanalkalay@msn.com>
2016-03-22 15:23:37 -0700

    worked on timetable and booking model

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -8,8 +8,45 @@
 
 class Timetable extends MY_Model {
     
-    protected $xml=null;    
-    protected $day=array();
-    protected $slot=array();
+    protected $xml=null; 
+    protected $timetable=null;
+  
+    protected $days = array();
+    protected $periods = array();
+    protected $courses = array();
     
+    
+    
+    	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->xml = simplexml_load_file(DATAPATH . 'timetable.xml');
+		
+                foreach ($this->xml->days->day as $day) {
+                    foreach ($day->slot as $slot) {
+                        
+                    $record = array();
+                }
+                }
+                
+		
+		
+		
+	}
+}
+
+ class Booking extends MY_Model
+{
+	public $whichday = "";
+	public $time = "";
+	public $whichcourse = "";
+	// Constructor
+	public function __construct($detail=null)
+	{
+		parent::__construct();
+		$this->whichday = (String) $detail['whichday'];
+		$this->time = (String) $detail['time'];
+		$this->whichcourse = (String) $detail['whichcourse'];
+	}
 }
